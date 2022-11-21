9f8a9c648da086745b76f3c559c27aefb51d4876
QWerty <118520899+NotQWertz@users.noreply.github.com>
2022-11-21 19:53:10 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,6 @@ const items = [
 
 const navItems = [
     { icon: 'fal fa-home', active: 'fa fa-home', label: 'Home', href: '/' },
-    { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projects', href: '/projects' },
-    { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'Contact', href: '/contact' },
 ]
 
 import Link from 'next/link';
